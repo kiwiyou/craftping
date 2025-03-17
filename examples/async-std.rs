adf3847c358b29d5f33f93b8b5ea44fbd45383a4
kiwiyou <kiwiyou@kiwiyou.dev>
2025-03-18 02:28:13 +0900

    fix(example): remove mineplex

--- examples/async-std.rs
+++ examples/async-std.rs
@@ -3,7 +3,7 @@ use craftping::futures::ping;
 
 #[async_std::main]
 async fn main() -> craftping::Result<()> {
-    let servers = ["us.mineplex.com", "mc.hypixel.net"];
+    let servers = ["mc.hypixel.net"];
     for &server in servers.iter() {
         let mut stream = TcpStream::connect((server, 25565)).await?;
         let response = ping(&mut stream, server, 25565).await?;
--- examples/servers.rs
+++ examples/servers.rs
@@ -3,7 +3,7 @@ use std::net::TcpStream;
 pub use craftping::sync::ping;
 
 fn main() {
-    let servers = ["us.mineplex.com", "mc.hypixel.net"];
+    let servers = ["mc.hypixel.net"];
     for &server in servers.iter() {
         let mut stream = TcpStream::connect((server, 25565)).unwrap();
         let response = ping(&mut stream, server, 25565).unwrap();
